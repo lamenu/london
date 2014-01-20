af1347b49c474dedf95373ebced57cae53182837
lamenu <lamenu2renner@yahoo.co.uk>
2014-01-20 21:29:37 +0000

    Update hi.java

--- good/src/good/hi.java
+++ good/src/good/hi.java
@@ -4,7 +4,7 @@ public class hi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("we are one");
+    System.out.println("we are one, yess");
 	}
 
 }
